58bdcc46a8fae41997808867ce701dbcacc2ae04
Muhamad-Amin <terbarubug7@gmail.com>
2023-08-12 15:23:05 +0700

    Belajar Python Dasar

---
+++ Project/tes.py
@@ -0,0 +1,13 @@
+print(15 * "=")
+print("Selamat Datang Di Perpustakaan Python")
+print(15 * "=")
+
+jumlah = []
+
+buku = input("Masukkan Judul Buku : ")
+penulis = input("Penulis : ")
+banyak = int(input("Banyak"))
+
+for i in range(0, banyak + 1):
+    jumlah.append(buku, penulis)
+print(jumlah)
